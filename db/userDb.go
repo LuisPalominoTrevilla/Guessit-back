78946b5d774889e15391fdc49772fe3bfa7e1908
Jorge Pietra Santa <jorgeps14@Jorges-MacBook-Pro.local>
2019-03-18 00:20:58 -0600

    added RegisterUser function

--- db/userDb.go
+++ db/userDb.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/LuisPalominoTrevilla/Guessit-back/models"
 	"github.com/mongodb/mongo-go-driver/bson"
@@ -21,4 +22,36 @@ func (db *UserDB) Get(filter bson.D, result *models.User) error {
 // Insert implements the InsertOne action in a model
 func (db *UserDB) Insert(user models.User) (*mongo.InsertOneResult, error) {
 	return db.Users.InsertOne(context.TODO(), user)
+}
+
+func (db *UserDB) RegisterUser(name string, lastname string, gender string, email string, username string, password string) error {
+	filter := bson.D{{"email", email}}
+
+	userToRegister := models.User{
+		Name:     name,
+		Username: username,
+		Image:    "",
+		Email:    email,
+		Gender:   gender,
+		LastName: lastname,
+		Password: password,
+		Age:      21,
+	}
+	fmt.Println("Trying to find user", name, lastname, "in db")
+
+	var foundUser models.User
+	err := db.Get(filter, &foundUser)
+
+	if err == mongo.ErrNoDocuments {
+		res, err := db.Insert(userToRegister)
+		if err != nil {
+			return err
+		}
+		fmt.Println("Added user", name, lastname, "to database", res.InsertedID)
+	} else if err == nil {
+		fmt.Println("Seed user already created with id ", foundUser.ID)
+	} else {
+		return err
+	}
+	return nil
 }
