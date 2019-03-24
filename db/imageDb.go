f0ef29ce8b4f44916f1760bd63345c1e9da130c4
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-03-23 20:20:13 -0600

    Added Get method to imageDB

--- db/imageDb.go
+++ db/imageDb.go
@@ -2,14 +2,39 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/LuisPalominoTrevilla/Guessit-back/models"
+	"github.com/mongodb/mongo-go-driver/bson"
 	"github.com/mongodb/mongo-go-driver/mongo"
 )
 
 // ImageDB serves as the wrapper for the image colelction
 type ImageDB struct {
 	Images *mongo.Collection
+}
+
+// Get retrieves filtered models from the database
+func (db *ImageDB) Get(filter bson.D) ([]*models.Image, error) {
+	cur, err := db.Images.Find(context.TODO(), filter)
+	fmt.Println("All good here")
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	var result []*models.Image
+
+	for cur.Next(context.TODO()) {
+		var image models.Image
+		err := cur.Decode(&image)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		result = append(result, &image)
+	}
+	return result, nil
 }
 
 // Insert implements the InsertOne action in a model
