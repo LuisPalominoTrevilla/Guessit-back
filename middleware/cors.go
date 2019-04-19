3d65d9e28db7696e378d6259f70fb43a0b84ab80
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-04-19 13:47:12 -0500

    Fixed cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -7,7 +7,8 @@ import (
 // Cors sets CORS to the requested resources
 func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Vary", "Origin")
 		w.Header().Set("Vary", "Access-Control-Request-Method")
 		w.Header().Set("Vary", "Access-Control-Request-Headers")
